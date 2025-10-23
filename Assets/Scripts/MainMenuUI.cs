3a7a59a54d1697105008a4de2c434062fc7290d9
DNupoor <dode@usc.edu>
2025-10-22 22:04:18 -0700

    Added Initial Instruction Screen

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -5,7 +5,7 @@ public class MainMenuUI : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene"); 
+        SceneManager.LoadScene("InstructionScene"); 
     }
 
     public void OpenTutorial()
