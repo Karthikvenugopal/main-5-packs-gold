0bde8d828bb116bcd278ffd170b1b6fe5dcd6552
fnusaloni <saloni@usc.edu>
2025-11-14 01:44:46 -0800

    fixed speed of guns

--- Assets/Scripts/CannonHazard.cs
+++ Assets/Scripts/CannonHazard.cs
@@ -82,6 +82,14 @@ public class CannonHazard : MonoBehaviour
         ApplyVariantStyling();
     }
 
+    public void OverrideProjectileSpeed(float speed)
+    {
+        if (speed > 0f)
+        {
+            projectileSpeed = speed;
+        }
+    }
+
     private void Fire()
     {
         GameObject projectileGO;
